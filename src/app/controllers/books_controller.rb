640d1a1df956eaa8295ccc1204587abc851cbf26
RyojiSubAC <ishitakeryo@gmail.com>
2020-04-13 15:54:28 +0900

    [update]View周りを修正

--- src/app/controllers/books_controller.rb
+++ src/app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   # GET /books.json
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   # GET /books/1
@@ -31,7 +32,8 @@ class BooksController < ApplicationController
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
       else
-        format.html { render :new }
+        @books = Book.all
+        format.html { render :index }
         format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
