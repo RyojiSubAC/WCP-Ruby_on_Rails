a87c6d7886c826eb5b793e641f5831272934e1c9
RyojiSubAC <ishitakeryo@gmail.com>
2020-04-17 14:39:52 +0900

    [add]課題2用のRspecファイルを追加&更新

--- src/spec/spec_helper.rb
+++ src/spec/spec_helper.rb
@@ -14,12 +14,17 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'capybara/rspec'
-#Capybara.javascript_driver = :webkit
+Capybara.register_driver :selenium_chrome_headless do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new()
+  browser_options.args << '--headless'
+  browser_options.args << '--no-sandbox'
+  browser_options.args << '--disable-gpu'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
 RSpec.configure do |config|
 	config.before(:each, type: :system) do
-    #driven_by :selenium_chrome_headless
-    driven_by :rack_test
-  end
+		driven_by :selenium_chrome_headless
+	end
 	# config.after(:suite) do
 	# 	DatabaseCleaner.strategy = :transaction
 	# 	DatabaseCleaner.clean_with(:truncation)
