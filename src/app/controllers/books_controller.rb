0518ec0d24b3b41655387b44fe0924dd6270961f
RyojiSubAC <ishitakeryo@gmail.com>
2020-04-17 14:35:13 +0900

    [update]一覧・作成・編集のロジックを修正

--- src/app/controllers/books_controller.rb
+++ src/app/controllers/books_controller.rb
@@ -5,36 +5,40 @@ class BooksController < ApplicationController
   # GET /books.json
   def index
     @books = Book.all
-    @book = Book.new
+    @new_book = Book.new
+    @user = current_user
   end
 
   # GET /books/1
   # GET /books/1.json
   def show
     @book = Book.find(params[:id])
-  end
-
-  # GET /books/new
-  def new
-    @book = Book.new
+    @new_book = Book.new
+    @user = @book.user
   end
 
   # GET /books/1/edit
   def edit
     @book = Book.find(params[:id])
+		if @book.user_id != current_user.id
+	    	redirect_to books_path
+	  end
+    @new_book = Book.new
   end
 
   # POST /books
   # POST /books.json
   def create
     @book = Book.new(book_params)
-
+    @book.user_id = current_user.id
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
       else
         @books = Book.all
+        @user = current_user
+        @new_book = @book
         format.html { render :index }
         format.json { render json: @book.errors, status: :unprocessable_entity }
       end
@@ -75,6 +79,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :body)
+      params.require(:book).permit(:title, :body, :user_id)
     end
 end
