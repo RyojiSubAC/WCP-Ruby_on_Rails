50af7bd236457d6a35d326cdca5fc58b28cd99a8
RyojiSubAC <ishitakeryo@gmail.com>
2020-04-17 14:32:48 +0900

    [update]バリデーション追加とBookモデルとのリレーションを設定

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ApplicationRecord
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :name, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 1対N
+  has_many :books
   attachment :profile_image
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
 end
