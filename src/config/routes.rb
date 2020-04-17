837ba7f36240c347152ac5fc140424b235f5653e
RyojiSubAC <ishitakeryo@gmail.com>
2020-04-17 14:38:06 +0900

    [update]ルーティングを更新

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
-  resources :books, except: :new 
+  resources :books, except: :new
+  resources :users, except: :new
+  get 'home/about' => 'home#about'
 end
