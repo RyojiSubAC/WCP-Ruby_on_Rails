4e41552eae4af7521ffa13b6b9b3cd6bdecc8517
RyojiSubAC <ishitakeryo@gmail.com>
2020-04-17 14:34:03 +0900

    [update]Refile.secret.keyを設定

--- src/config/initializers/application_controller_renderer.rb
+++ src/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f7b0eabb66a1ee7d1e66b6f687acab6659fb87862b7bc450868fc1db8103522e1c806e5915f899ee7bbd077c580809be752becf09a8c8840e6b9fde8f7c0dd02'
